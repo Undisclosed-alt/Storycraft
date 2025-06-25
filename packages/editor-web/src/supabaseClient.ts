61f49c4df5dfed046bdab942748bfd6dae15d8da
Undisclosed-alt <undisclosed.alt09@gmail.com>
2025-06-25 13:56:12 +0200

    fix build by aliasing supabase functions

--- packages/editor-web/src/supabaseClient.ts
+++ packages/editor-web/src/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import * as functions from '../../supabase/functions';
+import * as functions from '@supabase-functions';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
